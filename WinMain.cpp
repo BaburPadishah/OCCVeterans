4588cff448729b86f1552d385093f02a4ca3974d
BaburPadishah <51137224+BaburPadishah@users.noreply.github.com>
2019-06-04 11:27:14 -0700

    Update WinMain.cpp

--- WinMain.cpp
+++ WinMain.cpp
@@ -5,29 +5,23 @@
 #include <windows.h>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lparam);
+HWND AddControls(HWND); // Creates the static and edit controls, returns the handle to the edit control.
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
 {
-	// Registration
-	const wchar_t CLASS_NAME[] = L"OCC Veterans' Club Sign-In";
-	HWND textBox;
+	// Window Class Registration
+	const wchar_t CLASS_NAME[] = L"OCC Student Veterans Association Sign-In";
 
 	WNDCLASS wc = { };
-	WNDCLASS wcChild = { };
 
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
 	wc.lpszClassName = CLASS_NAME;
 
-	wcChild.lpfnWndProc = WindowProc;
-	wcChild.hInstance = hInstance;
-	wcChild.lpszClassName = CLASS_NAME;
-
 	RegisterClass(&wc);
-	RegisterClass(&wcChild);
 
 	// Window Creation
-	HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"OCC Veterans' Club Sign-In", WS_SYSMENU | !WS_MAXIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, 500, 200, NULL, NULL, hInstance, NULL);
+	HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"OCC Student Veterans Association Sign-In", WS_SYSMENU | !WS_MAXIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, 500, 200, NULL, NULL, hInstance, NULL);
 
 	if (hwnd == NULL)
 	{
@@ -38,6 +32,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ PWSTR pCm
 	HMENU hMenu = GetSystemMenu(hwnd, FALSE);
 	EnableMenuItem(hMenu, SC_CLOSE, MF_GRAYED);
 
+	// Display Window
 	ShowWindow(hwnd, nCmdShow);
 
 	// Message Loop
@@ -54,18 +49,37 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
+	case WM_CREATE:
+		{
+			// call functions to display static edit controls
+			// store handle to edit control
+			HWND hEdit = AddControls(hwnd);
+			break;
+		}
+
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
+		break;
 
-	case WM_PAINT:
+	/*case WM_PAINT:
 		{
 			PAINTSTRUCT ps;
 			HDC hdc = BeginPaint(hwnd, &ps);
 			FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
 			EndPaint(hwnd, &ps);
 		}
-	return 0;
+		return 0;
+		break;*/
+
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		break;
 	}
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+HWND AddControls(HWND hwnd)
+{
+	CreateWindow(L"STATIC", L"Please enter your ID below:", WS_VISIBLE | WS_CHILD | SS_CENTER, 155, 50, 190, 20, hwnd, NULL, NULL, NULL);
+	return CreateWindow(L"EDIT", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 100, 90, 300, 20, hwnd, NULL, NULL, NULL);
 }
