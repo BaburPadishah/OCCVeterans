33e2b9f01ed5c78bc1e320c5a2dfc2d7b7209fe7
BaburPadishah <51137224+BaburPadishah@users.noreply.github.com>
2019-06-25 20:23:10 -0700

    Added Changelog, User Manual, and sql database

---
+++ login_data.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE occ_veteran_club;
+
+USE occ_veteran_club;
+
+CREATE TABLE IF NOT EXISTS members(
+	id VARCHAR(8) PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    branch VARCHAR(15) NOT NULL
+    );
+    
+CREATE TABLE IF NOT EXISTS logins(
+	id VARCHAR(8) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    date_time DATETIME NOT NULL
+    );
