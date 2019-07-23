2bd4efe040cbb31c09f5395d94a3cfd3fe3781eb
BaburPadishah <51137224+BaburPadishah@users.noreply.github.com>
2019-07-23 09:24:09 -0700

    Added separate compilation, started newMember
    newMember does not work in its current form. It only produces a gray box instead of a window. However, it appears that this gray box can contain static controls.

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,36 +1,8 @@
-#ifndef UNICODE
-#define UNICODE
-#endif
-
-#include <windows.h>
-#include <mysql.h>
-#include <string>
-#include <iostream>
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lparam);
-LRESULT APIENTRY EditSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-void checkIdNum(HWND hwnd, WPARAM wParam);
-void AddControls(HWND);
-std::string QueryDB(LPSTR data, const SYSTEMTIME &st);
-void newMember(LPSTR data, HWND hwnd);
-
+#include "functions.h"
 
 // Global Variables
 HWND hEdit;
 WNDPROC wpOrigEditProc;
-
-
-// Global Constants
-const int WIN_WIDTH = 300;
-const int WIN_HEIGHT = 200;
-const int STATIC_WIDTH = 140;
-const int STATIC_HEIGHT = 20;
-const int EDIT_WIDTH = 140;
-const int EDIT_HEIGHT = 20;
-const int BUTTON_WIDTH = 70;
-const int BUTTON_HEIGHT = 40;
-
-enum Controls { STATIC, EDIT, BUTTON };
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE,
 	_In_ PWSTR pCmdLine, _In_ int nCmdShow)
@@ -46,18 +18,14 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE,
 
 	RegisterClass(&wc);
 
-	// Get screen resolution
-	int nScreenWidth = GetSystemMetrics(SM_CXSCREEN);
-	int nScreenHeight = GetSystemMetrics(SM_CYSCREEN);
-
 	// Window Creation
 	HWND hwnd = CreateWindowEx(
 		0,
 		CLASS_NAME,
 		L"OCC Student Veterans Association Sign-In",
 		NULL,
-		nScreenWidth / 2 - WIN_WIDTH / 2,
-		nScreenHeight / 2 - WIN_HEIGHT / 2,
+		SCREEN_W / 2 - WIN_WIDTH / 2,
+		SCREEN_H / 2 - WIN_HEIGHT / 2,
 		WIN_WIDTH,
 		WIN_HEIGHT,
 		NULL,
@@ -92,7 +60,6 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_CREATE:
 	{
 		AddControls(hwnd);
-		wpOrigEditProc = (WNDPROC)SetWindowLongPtr(hEdit, GWLP_WNDPROC, (LONG_PTR)EditSubclassProc);
 		break;
 	}
 
@@ -140,6 +107,41 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
+LRESULT CALLBACK RegWinProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		AddRegControls(hwnd);
+		return 0;
+		break;
+
+	//disables dragging
+	case WM_NCLBUTTONDOWN:
+		if (wParam == HTCAPTION)
+		{
+			return 0;
+		}
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+		break;
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_MENU + 1));
+		EndPaint(hwnd, &ps);
+	}
+	return 0;
+	break;
+	}
+}
+
 // Adds button and static/edit controls
 void AddControls(HWND hwnd)
 {
@@ -147,7 +149,8 @@ void AddControls(HWND hwnd)
 		L"Please enter your ID:",
 		WS_VISIBLE | WS_CHILD | SS_CENTER,
 		WIN_WIDTH / 2 - STATIC_WIDTH / 2,
-		WIN_HEIGHT / 10, STATIC_WIDTH,
+		WIN_HEIGHT / 10, 
+		STATIC_WIDTH,
 		STATIC_HEIGHT,
 		hwnd,
 		(HMENU)STATIC,
@@ -174,16 +177,65 @@ void AddControls(HWND hwnd)
 		L"",
 		WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL,
 		WIN_WIDTH / 2 - EDIT_WIDTH / 2,
-		WIN_HEIGHT / 4, EDIT_WIDTH,
+		WIN_HEIGHT / 4,
+		EDIT_WIDTH,
 		EDIT_HEIGHT,
 		hwnd,
 		(HMENU)EDIT,
 		NULL,
 		NULL
 	);
+
+	wpOrigEditProc = (WNDPROC)SetWindowLongPtr(hEdit, GWLP_WNDPROC, (LONG_PTR)EditSubclassProc);
 }
 
-//Verifys that the ID number given is a valid one
+void AddRegControls(HWND parent)
+{
+	// Three static fields: ID, name, branch
+	CreateWindow(L"STATIC",
+		L"Student ID:",
+		WS_VISIBLE | WS_CHILD | SS_CENTER,
+		WIN_WIDTH / 2 - STATIC_WIDTH / 2,
+		WIN_HEIGHT / 10, 
+		STATIC_WIDTH,
+		STATIC_HEIGHT,
+		parent,
+		(HMENU)REG_ID_STATIC,
+		NULL,
+		NULL
+	);
+
+	CreateWindow(L"STATIC",
+		L"First and Last Name:",
+		WS_VISIBLE | WS_CHILD | SS_CENTER,
+		WIN_WIDTH / 2 - STATIC_WIDTH / 2,
+		WIN_HEIGHT / 2,
+		STATIC_WIDTH,
+		STATIC_HEIGHT,
+		parent,
+		(HMENU)REG_NAME_STATIC,
+		NULL,
+		NULL
+	);
+
+	CreateWindow(L"STATIC",
+		L"Branch:",
+		WS_VISIBLE | WS_CHILD | SS_CENTER,
+		WIN_WIDTH / 2 - STATIC_WIDTH / 2,
+		WIN_HEIGHT / 4,
+		STATIC_WIDTH,
+		STATIC_HEIGHT,
+		parent,
+		(HMENU)REG_BRANCH_STATIC,
+		NULL,
+		NULL
+	);
+	// Two edit fields: ID (filled by buffer), name
+	// Five radio buttons: Army, Navy, Air Force, Marines, Coast Guard
+	// Two buttons: Register, Cancel
+}
+
+//Verifies that the ID number given is a valid one
 void checkIdNum(HWND hwnd, WPARAM wParam)
 {
 	if (wParam == BUTTON || wParam == VK_RETURN)
@@ -212,9 +264,9 @@ void checkIdNum(HWND hwnd, WPARAM wParam)
 				else
 					DestroyWindow(GetParent(hwnd));
 			}
-			else if (len >= 7 && len <= 9 && allDigit) // user has entered an ID number
+			else if ((len == 7 || (len == 8 && buffer[0] == '0') || (len == 9 && buffer[0] == 'C')) && allDigit) // user has entered an ID number
 			{
-				if (!isdigit(buffer[0]))
+				if (len == 9)
 				{
 					++buffer; // removes 'C' at beginning of input, if user enters it
 				}
@@ -230,7 +282,7 @@ void checkIdNum(HWND hwnd, WPARAM wParam)
 						L"ID not found. Would you like to register as a new member?", 
 						L"New Member", MB_YESNO) == IDYES)
 					{
-						//TODO: newMember function
+						newMember(buffer, hwnd);
 					}
 				}
 				else // found a result
@@ -258,69 +310,4 @@ LRESULT APIENTRY EditSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lP
 		}
 	}
 	return CallWindowProc(wpOrigEditProc, hwnd, uMsg, wParam, lParam);
-}
-
-//connect to and query database
-std::string QueryDB(LPSTR data, const SYSTEMTIME &st)
-{
-	MYSQL* conn;
-	MYSQL_ROW row;
-	MYSQL_RES* res;
-	int qstate;
-	conn = mysql_init(0);
-
-	conn = mysql_real_connect(
-		conn, 
-		"localhost", 
-		"root", 
-		"Garamantes45!", 
-		"occ_veteran_club", 
-		3306, 
-		nullptr, 
-		0
-	);
-
-	if (!conn)
-	{
-		return 0;
-	}
-
-	std::string result = "not found";
-	std::string query = "SELECT * FROM members WHERE id = " 
-		+ static_cast<std::string>(data);
-	const char* q = query.c_str();
-
-	qstate = mysql_query(conn, q);
-
-	if (!qstate) // mysql_query functioned correctly
-	{
-		res = mysql_store_result(conn);
-		while (row = mysql_fetch_row(res))
-			result = row[1];
-		mysql_free_result(res);
-	}
-	else
-	{
-		std::cerr << "Query failed: " << mysql_error(conn) << std::endl;
-	}
-
-	if (result != "not found")
-	{
-		char datetime[21];
-		sprintf_s(datetime, "%d-%02d-%02d %02d:%02d:%02d", 
-			st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
-
-		std::string ins = "INSERT INTO logins (id, name, date_time) VALUES (" 
-			+ static_cast<std::string>(data) 
-			+ ", '" + result + "', " + "'" + datetime + "')";
-
-		qstate = mysql_query(conn, ins.c_str());
-
-		if (qstate)
-		{
-			std::cerr << "Query failed: " << mysql_error(conn) << std::endl;
-		}
-	}
-
-	return result;
 }
